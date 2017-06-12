c829fccf241ad4404c65581aa3764ec8c6d23e60
Hector Garcia <hectorglara@outlook.com>
2017-06-12 18:38:55 +0000

    Creating db file to control connection to MongoDb

---
+++ server/db.js
@@ -0,0 +1,34 @@
+var MongoClient = require('mongodb').MongoClient, 
+    assert = require('assert'),
+    util = require('util');
+
+var state = {
+    db: null
+}
+
+// Connection URL
+var url = 'mongodb://admin:admin1234@ds115712.mlab.com:15712/nba-bracket-competition';
+
+// Use connect method to connect to the server
+exports.connect = function(){
+    MongoClient.connect(url, (err, database) => {
+      if (err) 
+        return console.log(err);
+      
+      state.db = database;
+    });
+};
+
+exports.get = function() {
+  return state.db
+}
+
+exports.close = function(done) {
+  if (state.db) {
+    state.db.close(function(err, result) {
+      state.db = null
+      state.mode = null
+      done(err)
+    })
+  }
+}
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,30 +1,19 @@
 const express = require('express');
 const router = express.Router();
+var db = require('../db');
 
-var MongoClient = require('mongodb').MongoClient, 
-    assert = require('assert'),
-    util = require('util');
-
-var db;
-
-// Connection URL
-var url = 'mongodb://admin:admin1234@ds115712.mlab.com:15712/nba-bracket-competition';
-
-// Use connect method to connect to the server
-MongoClient.connect(url, (err, database) => {
-  if (err) 
-    return console.log(err);
-  
-  db = database;
-});
+db.connect();
 
 //Get api listing
-router.get('/', (req, res) => {
-    db.collection('teams').find().toArray((err, result) => {
+router.get('/teams', (req, res) => {
+    var collection = db.get().collection('teams');
+    
+    collection.find().toArray((err, result) => {
     if (err) return console.log(err);
     
     res.send(result);
   });
+
 });
 
 router.post('/:urlparam', function(req, res) {
@@ -33,24 +22,15 @@ router.post('/:urlparam', function(req, res) {
   // checkBody only checks req.body; none of the other req parameters
   // Similarly checkParams only checks in req.params (URL params) and
   // checkQuery only checks req.query (GET params).
-  req.checkBody('postparam', 'Invalid postparam').notEmpty().isInt();
+  req.checkBody({
+    'contact.email': {
+      isEmail: {
+        errorMessage: 'Invalid Email'
+      }
+    }
+  });
   req.checkParams('urlparam', 'Invalid urlparam').isAlpha();
   req.checkQuery('getparam', 'Invalid getparam').isInt();
-
-  // OR assert can be used to check on all 3 types of params.
-  // req.assert('postparam', 'Invalid postparam').notEmpty().isInt();
-  // req.assert('urlparam', 'Invalid urlparam').isAlpha();
-  // req.assert('getparam', 'Invalid getparam').isInt();
-
-  // SANITIZATION
-  // as with validation these will only validate the corresponding
-  // request object
-  req.sanitizeBody('postparam').toBoolean();
-  req.sanitizeParams('urlparam').toBoolean();
-  req.sanitizeQuery('getparam').toBoolean();
-
-  // OR find the relevent param in all areas
-  req.sanitize('postparam').toBoolean();
 
   // Alternatively use `var result = yield req.getValidationResult();`
   // when using generators e.g. with co-express
@@ -62,7 +42,7 @@ router.post('/:urlparam', function(req, res) {
     res.json({
       urlparam: req.params.urlparam,
       getparam: req.query.getparam,
-      postparam: req.body.postparam
+      body: req.body
     });
   });
 });
